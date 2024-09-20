3a34930563d095a05674c3a55c475eb577f18845
Sebastian-Fon <sebastian.fonseca@correo.ucu.edu.uy>
2024-09-20 16:29:33 -0300

    interfaz IItem y clase item defensa

---
+++ src/Library/IItem defensa.cs
@@ -0,0 +1,8 @@
+﻿namespace Library;
+
+public interface IItemDefensa
+{
+    string NombreItem();
+    int ValorDefensa();
+    
+}
---
+++ src/Library/Item defensa.cs
@@ -0,0 +1,23 @@
+﻿namespace Library;
+
+public class ItemDefensa:IItemDefensa
+{
+    public string nombreItem { get; set; }
+    public int valorDefensa { get; set; }
+
+    public ItemDefensa(string nombreItem, int valorDefensa)
+    {
+        this.nombreItem = nombreItem;
+        this.valorDefensa = valorDefensa;
+    }
+
+    public string NombreItem()
+    {
+        return nombreItem;
+    }
+
+    public int ValorDefensa()
+    {
+        return valorDefensa;
+    }
+}
